e70c15cc895ee9fbc790165135b278591e3eda66
kg233 <38277731+kg233@users.noreply.github.com>
2019-06-27 18:16:20 -0700

    Add files via upload

---
+++ cpp data structures and algorithms/project2/out/CMakeFiles/othelloaistudent.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/othelloaistudent.dir/core/Ai3.cpp.o"
+  "CMakeFiles/othelloaistudent.dir/core/Ai2.cpp.o"
+  "lib/libothelloaistudent.pdb"
+  "lib/libothelloaistudent.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/othelloaistudent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
